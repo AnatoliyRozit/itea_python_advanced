6e2aacd6298800d0216b36f8713a96c39f0f7374
AnatoliyRozit <grimuar0@gmail.com>
2020-05-15 09:50:17 +0300

    class_tasks_05 Added Context Manager

---
+++ 05/my_context_manager.py
@@ -0,0 +1,18 @@
+class MyContextManager:
+
+    def __init__(self, name, mode):
+        self.obj = open(name, mode)
+
+    def __enter__(self):
+        return self.obj
+
+    def __exit__(self, type, value, traceback):
+        print('Oops! Something Went Wrong. Check your entry and try again.')
+        self.obj.close()
+        return True
+
+
+if __name__ == '__main__':
+
+    with MyContextManager('some_file.txt', 'w') as f:
+        f.write('Hi!')
